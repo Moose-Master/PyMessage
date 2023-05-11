e2b59d0e467bbe05ba56cd74c15c950a36872ae7
Moose-Master <korivan888@gmail.com>
2023-05-11 08:17:13 -0500

    Removed Broken Name Code

--- client.py
+++ client.py
@@ -3,16 +3,7 @@ import threading
 import forcebytes
 HOST = "68.168.164.146"  # The server's hostname or IP address
 PORT = 65433  # The port used by the server
-names = []
 def send():
-    name = input("Name? ")
-    for i in range(len(names)):
-        if names[i] == name:
-             print("That name is taken")
-             exit()
-        else:
-             print("👍")    
-    s.sendall(str.encode(name))
     while True:
         print("Ready")
         mesg = str.encode(name +": " + input(),"utf-8")
@@ -27,11 +18,7 @@ def get():
 receaving = threading.Thread(target=get)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    #nameslen = int(s.recv(1024))
-    nameslen = int.from_bytes(forcebytes.fr(s,4), byteorder='big')
-    for i in range(nameslen):
-        namelen = int.from_bytes(forcebytes.fr(s,4), byteorder='big')
-        names.append((forcebytes.fr(s,namelen)).decode('utf-8'))
+    name = input("Name? ")
     sending.start()
     receaving.start()
     receaving.join()
--- server.py
+++ server.py
@@ -2,25 +2,14 @@ import socket
 import threading
 import forcebytes
 connections = []
-names = []
-nameslen = len(names)
 def multi_client(conn):
                 print(f"Connected by {addr}")
-                conn.sendall((int(nameslen)).to_bytes(4,'big'))
-                for w in range(len(names)):
-                        conn.sendall((int(len(names[w]))).to_bytes(4,'big'))
-                        conn.sendall(str.encode(names[w]))
-                try:
-                        names.append(conn.recv(1024).decode("ascii"))
-                except Exception:
-                        return
-                print(names)
                 while True:
                         try:
                                 data = conn.recv(1024)
                         except Exception:
                                 return
-                        print(f"Server received {data!r}")
+                        print(data.decode('utf-8'))
                         for c in connections:
                                 if c != conn:                              
                                         try:
